7b093f0c201f82dc917ae0311c4f9000c65df40e
Jonasz Balinski <->
2022-03-25 11:13:39 +0100

    Add project files.

---
+++ database/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace database
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            PERSON newPerson = new PERSON();
+            newPerson.name = "Henry";
+            newPerson.date = DateTime.Parse("07.07.2002");
+
+            DatabaseDataContext dt = new DatabaseDataContext();
+            dt.PERSONs.InsertOnSubmit(newPerson);
+            dt.SubmitChanges();
+
+            foreach (PERSON p in dt.PERSONs)
+            {
+                MessageBox.Show(p.name);
+            }
+        }
+    }
+}
